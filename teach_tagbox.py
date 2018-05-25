4b75f9367aa93ac941ef60d8e915fdd949f1ac96
robmarkcole <robmarkcole@gmail.com>
2018-05-25 19:39:49 +0100

    More tidy

--- teach_tagbox.py
+++ teach_tagbox.py
@@ -4,6 +4,7 @@ import requests
 IP = 'localhost'
 PORT = '8080'
 CLASSIFIER = 'tagbox'
+VALID_FILETYPES = ('.jpg', '.png', '.jpeg')
 
 teach_api_url = "http://{}:{}/{}/teach".format(IP, PORT, CLASSIFIER)
 health_api_url = "http://{}:{}/readyz".format(IP, PORT)
@@ -38,21 +39,23 @@ def test_classifier_health():
 
 def main():
     if test_classifier_health():
-        for folder in list_folders():
-            tag = folder
-            for file in os.listdir(os.getcwd() + "/" + tag):
-                if file.endswith(('.jpg', '.png', '.jpeg')):
+        for folder_name in list_folders():
+            folder_path = os.path.join(os.getcwd(), folder_name)
+            for file in os.listdir(folder_path):
+                if file.endswith(VALID_FILETYPES):
+                    file_path = os.path.join(folder_path, file)
+                    tag = folder_name
                     response = (requests.post(
                         teach_api_url,
                         data={'tag': tag, "id": file},
-                        files={'file': open(tag + '/' + file, 'rb')}
+                        files={'file': open(file_path, 'rb')}
                         ))
 
                     if response.status_code == 200:
-                        print("File: {} tagged with tag: {}".format(file, tag))
+                        print("File:{} tagged with tag:{}".format(file, tag))
 
                     elif response.status_code == 400:
-                        print("Tagging of file: {} failed with message: image ".format(
+                        print("Tagging of file:{} failed with message:".format(
                             file, response.text))
 
 
